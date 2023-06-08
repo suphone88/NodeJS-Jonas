805f55c0213658294ea0f48eb95e425883a18c77
Dev Su Su <suphonemyint.la@gmail.com>
2023-06-08 10:21:42 +0630

    add to node-farm

---
+++ node-farm/index2.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+// Blocking synchronous way
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File Written!");
