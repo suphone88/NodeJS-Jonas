7b7cd2483b6eb22773f228dbbfbb14447538dd77
Dev Su Su <suphonemyint.la@gmail.com>
2023-06-15 09:17:58 +0630

    Asynchronous JS

---
+++ 3-Asynchronous JS/index03.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file !!');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file !!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
