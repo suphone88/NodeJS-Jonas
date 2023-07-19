8344abd01055112cb1489924805b110b8043d62e
Dev Su Su <suphonemyint.la@gmail.com>
2023-07-20 02:13:35 +0630

    section 12 building the tour page

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -20,10 +20,12 @@ router
     tourController.getMonthlyPlan
   );
 
-router.route(
-  '/tours-within/:distance/center/:latlng/unit/:unit',
-  tourController.getToursWithin
-);
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+// /tours-within?distance=233&center=-40,45&unit=mi
+// /tours-within/233/center/-40,45/unit/mi
 
 //router.param('id', tourController.checkID);
 
